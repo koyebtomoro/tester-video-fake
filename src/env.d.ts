1ada23a5b43c6fdbc35b5249251d9e2b5964d60f
Saddan Bima Nagantara <bimanagantara@yahoo.com>
2025-02-16 20:40:17 +0700

    Update

---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_TELEGRAM_BOT_TOKEN: '7631972577:AAE6mYqCQ8loen0hrx0Nb84mRT3fB9vqUto'
+  readonly VITE_TELEGRAM_CHAT_ID: '6328979211'
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
