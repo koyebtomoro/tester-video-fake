8233001ddb32e73c6198f9d2878898428f1b67c1
koyebtomoro <koyebtomoro@gmail.com>
2025-02-17 21:59:59 +0700

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_TELEGRAM_BOT_TOKEN: '7631972577:AAE6mYqCQ8loen0hrx0Nb84mRT3fB9vqUto'
-  readonly VITE_TELEGRAM_CHAT_ID: '6328979211'
+  readonly VITE_TELEGRAM_BOT_TOKEN: '7960931911:AAGwHfP5iGklggaCbgizXAePA3U5sNaBbdU'
+  readonly VITE_TELEGRAM_CHAT_ID: '7575029713'
 }
 
 interface ImportMeta {
